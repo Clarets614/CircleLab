1543c04e8f5368e34096c735e6f414f6dee2486b
Clare S <Clare.Stock@gmail.com>
2024-05-10 17:05:50 -0400

    first commit

---
+++ Circle-oop-Lab/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_oop_Lab
+{
+    public class Circle
+    {
+        //properties
+        private double Radius { get; set; }
+
+        //Constructor
+
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+
+
+        //methods
+
+        public double CalculateDiameter()
+        {
+            double diameter = Radius * 2;
+            return diameter;
+        }
+
+        public double CalculateCircumference()
+        {
+            double circumference = (Radius * 2) * Math.PI;
+            return circumference;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Radius * Radius * Math.PI;
+            return area;
+        }
+
+        public void Grow()
+        {
+            Radius = Radius * 2;
+        }
+
+        public double GetRadius()
+        {
+            return Radius;
+        }
+    }
+}
---
+++ Circle-oop-Lab/Program.cs
@@ -0,0 +1,59 @@
+﻿//main program
+
+using Circle_oop_Lab;
+
+Console.WriteLine("Hello Welcome to the Circle of Methods");
+Console.WriteLine("Enter Radius:");
+
+//Validation loop
+double value = 0;
+while (true)
+{
+    try
+    {
+        value = double.Parse(Console.ReadLine());
+
+        //if successful
+        break;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Please enter a valid number");
+    }
+}
+//new circle object
+Circle userCircle = new Circle(value);
+Console.WriteLine($"Diameter:{userCircle.CalculateDiameter()}");
+Console.WriteLine($"Circumference : {userCircle.CalculateCircumference()}");
+Console.WriteLine($"Area: {userCircle.CalculateArea()}");
+
+bool growCircle = true;
+//asking user if circle should grow
+while (true)
+{
+    Console.WriteLine("\nWould you like the circle to grow?");
+    string choice = Console.ReadLine().ToLower().Trim();
+    if (choice == "y")
+    {
+        Console.WriteLine("The Circle grows larger.");
+        userCircle.Grow();
+        Console.WriteLine($"Diameter:{userCircle.CalculateDiameter()}");
+        Console.WriteLine($"Circumference : {userCircle.CalculateCircumference()}");
+        Console.WriteLine($"Area: {userCircle.CalculateArea()}");
+
+    }
+    else if (choice == "n")
+    {
+        growCircle = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("that answer is invalid, please type y to continue or n to quit");
+    }
+
+
+}
+
+Console.WriteLine($"The final radius of this circle is {userCircle.GetRadius()}.");
+Console.WriteLine("Thank you and have a nice day!");
